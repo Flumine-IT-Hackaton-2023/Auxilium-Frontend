b80a4ab4a3488d013c05c07c3a900a49b433a2ac
iamvirgoo <carens.kha4atryan@yandex.ru>
2023-05-28 10:34:07 +0300

    FIX: stylesheet for fonts

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,11 +5,13 @@ import './global.sass'
 import './styles/index.sass'
 import './styles/glitch.sass'
 import './styles/dropdown.sass'
+import '../public/fonts/schoonheid-font/stylesheet.css'
 
 import { RouterProvider } from "react-router-dom";
 import { Router } from "./router";
 import store from './store';
 import { Provider } from 'react-redux';
+
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
